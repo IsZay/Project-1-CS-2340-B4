c070a3c1c470ce45effb71e750281d2f247a8c87
Isaiah Shakir At Work <isaiahshakir02@gmail.com>
2025-01-29 16:48:48 -0500

    I'm about to take out line 3 on register.html to see what happens

--- gtMovies/views.py
+++ gtMovies/views.py
@@ -1,29 +1,36 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
 # Our views page actually shows what the consumer is viewing
 # a pretty convienient name
 def index(request):
     return HttpResponse("Hello, world. You're at the gtMovies index.")
 
+
 from django.contrib.auth.forms import UserCreationForm
+
 from django.shortcuts import render, redirect
+
 
 def register(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('login') # Redirect to login page after successful registration
+            return redirect('login')  # Redirect to login page after successful registration
     else:
         form = UserCreationForm()
     return render(request, 'gtMovies/register.html', {'form': form})
+
+
 # observe this last line. We haven't made gtMovies.register.html yet. This will be done later
 
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm
+
 
 def user_login(request):
     if request.method == "POST":
@@ -31,7 +38,7 @@ def user_login(request):
         if form.is_valid():
             user = form.get_user()
             login(request, user)
-            return redirect('home') # Redirect to home page after successful login
+            return redirect('home')  # Redirect to home page after successful login
     else:
         form = AuthenticationForm()
     return render(request, 'gtMovies/login.html', {'form': form})
