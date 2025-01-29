6e5153b8a876aea3cfc489eb6c73c5e484cbab8e
Isaiah Shakir At Work <isaiahshakir02@gmail.com>
2025-01-29 15:13:45 -0500

    Making the Movie class is coming along. thinking more about functionality with the login screen vs the register screen which you'll see on the urls page

--- gtMovies/models.py
+++ gtMovies/models.py
@@ -3,3 +3,74 @@ from django.db import models
 # Create your models here.
 
 #Pretty sure this is where I can add objects/classes
+
+# this is where we made the
+# class Question(models.Model):
+
+# I have no idea what that does but I'm gonna copy all the same imports
+
+
+# Create your models here.
+
+import datetime
+
+from django.db import models
+from django.utils import timezone
+from django.contrib import admin
+
+#
+# class Question(models.Model):
+#     question_text = models.CharField(max_length=200)
+#     pub_date = models.DateTimeField("date published")
+#     def __str__(self):
+#         return self.question_text
+#     def was_published_recently(self):
+#         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+#
+#     @admin.display(
+#         boolean=True,
+#         ordering="pub_date",
+#         description="Published recently?",
+#     )
+#     def was_published_recently(self):
+#         now = timezone.now()
+#         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+#
+# class Choice(models.Model):
+#     question = models.ForeignKey(Question, on_delete=models.CASCADE)
+#     choice_text = models.CharField(max_length=200)
+#     votes = models.IntegerField(default=0)
+#     def __str__(self):
+#         return self.choice_text
+#
+# def was_published_recently(self):
+#     now = timezone.now()
+#     return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+#this page does not work at all
+#Okay PyCharm AI filled this in for me
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    year = models.IntegerField()
+    director = models.CharField(max_length=200)
+    genre = models.CharField(max_length=200)
+    description = models.TextField()
+    rating = models.DecimalField(max_digits=3, decimal_places=2)
+    # poster = models.ImageField(upload_to='posters/') # Cannot use ImageField because Pillow is not installed
+
+    @admin.display(
+        #lets show something only admin should see
+        boolean=True, # this obviously doesn't work logically
+        ordering="title", #order the tings by the variable "title"  ideally
+        description="Published recently?", # I want to sort it by title, and then output the year?? Why??
+
+    )
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+#
+# def __str__(self):
+#         return self.title
+
+#Alr it finishe
--- gtMovies/urls.py
+++ gtMovies/urls.py
@@ -7,7 +7,9 @@ from .views import register, user_login
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.index, name="index"), #this should have all of our movie lists together, then have a login button in the top right and a register button.
+    # we can have a login button, and there it has a "register here" in there like most websites, which wouldn't be hard to change the path underneath as you'll see ***
+    #'login/register' actually that doesn't make sense
     path('register/', register, name='register'),
     path('login/', user_login, name='login'),
     path('logout/', LogoutView.as_view(next_page='home'), name='logout'),
