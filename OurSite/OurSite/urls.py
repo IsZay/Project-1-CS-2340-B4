c8b2631f726cc281ab6872c037a860511d58144a
Isaiah Shakir <isaiahshakir02@gmail.com>
2025-01-29 00:09:54 -0500

    Finished DjangoTutorial Setup. It doesn't start making a polls app at this point, but doesn't go deep enough into making admin. Will have to finish later

--- OurSite/OurSite/urls.py
+++ OurSite/OurSite/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib import admin
+from django.urls import path,include
+
 
 urlpatterns = [
+    path("gtMovies/", include("gtMovies.urls")),
     path('admin/', admin.site.urls),
 ]
