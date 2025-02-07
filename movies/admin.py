889aa249f2781695930edd9ebe5c38002c561ee1
Pjones97 <168655932+Pjones97@users.noreply.github.com>
2025-02-07 04:44:50 -0500

    movie upload, format fix, database update
    i RE uploaded movies and created the SQL data base. hoping that will stop the deletion of our data. i fixed the formatting and reviews.

--- movies/admin.py
+++ movies/admin.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 from .models import Movie, Review
 class MovieAdmin(admin.ModelAdmin):
     ordering = ['title'] #Maybe id might look better?
-    search_fields = ['name']
+    search_fields = ['title']
 admin.site.register(Movie, MovieAdmin)
 admin.site.register(Review)
 
