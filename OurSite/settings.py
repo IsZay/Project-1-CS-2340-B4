9fc307b661a4d49a0c7217d7c09985147312f50f
Tayo <tayo.ojo0128@gmail.com>
2025-02-10 17:26:47 -0500

    Freedom at last

--- OurSite/settings.py
+++ OurSite/settings.py
@@ -84,13 +84,9 @@ WSGI_APPLICATION = 'OurSite.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'moviestore',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
+     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
