d9d993508bd1d0d4072686c740df2af32e4aa4ea
Isaiah Shakir At Work <isaiahshakir02@gmail.com>
2025-02-12 11:10:53 -0500

    Finished Chapter 11

--- cart/models.py
+++ cart/models.py
@@ -12,3 +12,19 @@ class Order(models.Model):
         on_delete=models.CASCADE)
     def __str__(self):
         return str(self.id) + ' - ' + self.user.username
+
+
+from django.db import models
+from django.contrib.auth.models import User
+from movies.models import Movie
+
+class Item(models.Model):
+    id = models.AutoField(primary_key=True)
+    price = models.IntegerField()
+    quantity = models.IntegerField()
+    order = models.ForeignKey(Order,
+        on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie,
+        on_delete=models.CASCADE)
+    def __str__(self):
+        return str(self.id) + ' - ' + self.movie.title
